9532c79a2ad9ea198622ada7ff3f08240b685d49
amnayounus <amnayounus999@gmail.com>
2024-04-07 22:51:01 +0500

    restaurant_detail1

--- desi_dhaba/app/(routes)/restaurants/[name]/page.jsx
+++ desi_dhaba/app/(routes)/restaurants/[name]/page.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client"
+import GlobalApi from '@/app/_utils/GlobalApi'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import Intro from '../_components/Intro';
 
 function RestaurantDetails() {
+
+  const param=usePathname(); //show current path name
+  const [restaurant,setRestaurant]=useState([])
+  useEffect(()=>{
+    GetRestaurantDetail(param.split("/")[2])
+  }, [])
+  const GetRestaurantDetail=(restroSlug)=>{
+    GlobalApi.GetBusinessDetail(restroSlug).then(resp=>{
+      console.log(resp)
+      setRestaurant(resp.restaurant);
+    })
+  }
+
+
   return (
     <div>
-          RestaurantDetails
+    <Intro restaurant={restaurant}/>
     </div>
   )
 }
