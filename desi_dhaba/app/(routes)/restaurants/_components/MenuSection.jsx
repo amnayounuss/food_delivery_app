78d35971a1710ebc295f27a7bbb08d6ff38cfc56
amnayounus <amnayounus999@gmail.com>
2024-04-08 14:11:06 +0500

    MenuUpdated

--- desi_dhaba/app/(routes)/restaurants/_components/MenuSection.jsx
+++ desi_dhaba/app/(routes)/restaurants/_components/MenuSection.jsx
@@ -1,17 +1,26 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const MenuSection = ({restaurant}) => {
+    const FilterMenu= ({category})=>{
+        const result = restaurant?.menu?.filter((item)=>item.category==category)
+        console.log(result)
+    }
   return (
-    <div>
       <div className='grid grid-cols-4 mt-2'>
-        <div className='hidden md:flex'>
-            Category
+        <div className='hidden md:flex flex-col mr-10 g-2'>
+            {restaurant?.menu?.map((item,index)=>(
+                <Button 
+                className="flex justify-start"
+                onClick={()=>FilterMenu(item.category)}
+                variant="ghost" 
+                key={index}>{item.category}</Button>
+            ))}
         </div>
         <div className='col-span-3'>
             Menu List
         </div>
       </div>
-    </div>
   )
 }
 
