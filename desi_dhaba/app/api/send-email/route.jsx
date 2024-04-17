91aa52baefb899be8130627c15de4673e661a93d
faheem5630 <faheemriazmasih@gmail.com>
2024-04-17 14:52:47 +0500

    email1

--- desi_dhaba/app/api/send-email/route.jsx
+++ desi_dhaba/app/api/send-email/route.jsx
@@ -1,4 +1,4 @@
-import Email from "@/emails";
+import { Email } from "@/emails";
 import { data } from "autoprefixer";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
@@ -9,14 +9,11 @@ const resend=new Resend(process.env.RESEND_API_KEY)
 export async function POST(req){
     const response=await req.json(data);
     try{
-
-        
-
         const data =  await resend.emails.send({
             from: 'DesiDhaba@desidhaba.com',
             to: [response.email],
             subject: 'Desi Dhaba Confirmation Order',
-            react: Email(),
+            react:Email(),
         });
         return NextResponse.json({data})
 
