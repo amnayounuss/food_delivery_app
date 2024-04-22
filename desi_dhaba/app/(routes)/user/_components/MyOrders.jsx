08405ee336401b3985a9ef07f1130f3a126bac6b
amnayounus <amnayounus999@gmail.com>
2024-04-22 22:44:35 +0500

    UserProfile

--- desi_dhaba/app/(routes)/user/_components/MyOrders.jsx
+++ desi_dhaba/app/(routes)/user/_components/MyOrders.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+// MyOrders.js
+import GlobalApi from '@/app/_utils/GlobalApi';
+import { useUser } from '@clerk/nextjs';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 
 const MyOrders = () => {
+  const { user } = useUser();
+  const [orderList, setOrderList] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      GetUserOrders();
+    }
+  }, [user]);
+
+  const GetUserOrders = async () => { 
+    try {
+      const resp = await GlobalApi.GetUserOrders(user?.primaryEmailAddress.emailAddress); 
+      setOrderList(resp?.orders); 
+    } catch (error) {
+      console.error("Error fetching user orders:", error); 
+    }
+  };
+
   return (
     <div>
-      
+      <h2 className='font-bold text-lg'>My Orders</h2> 
+      <div>
+        {orderList && orderList.length > 0 ? ( 
+          orderList.map((order, index) => (
+            <div key={index}>
+              <h2>{moment(order.createdAt).format('DD-MMM-yyyy')}</h2>
+            </div>
+          ))
+        ) : (
+          <p>No orders found.</p> 
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyOrders
+export default MyOrders;
--- desi_dhaba/app/_utils/GlobalApi.js
+++ desi_dhaba/app/_utils/GlobalApi.js
@@ -2,8 +2,8 @@ const { gql, default: request } = require("graphql-request");
 
 const MASTER_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
-const GetCategory = async() => {
-    const query = gql `
+const GetCategory = async () => {
+  const query = gql`
     query Categories {
         categories(first: 50) {
           id
@@ -17,12 +17,12 @@ const GetCategory = async() => {
       
     `
 
-    const result = await request(MASTER_URL, query);
-    return result;
+  const result = await request(MASTER_URL, query);
+  return result;
 }
 
-const GetBusiness = async(category) => {
-    const query = gql `
+const GetBusiness = async (category) => {
+  const query = gql`
     query GetBusiness {
   restaurants(where: {categories_some: {slug: "` + category + `"}}) {
     aboutUs
@@ -47,12 +47,12 @@ const GetBusiness = async(category) => {
 
   
   `
-    const result = await request(MASTER_URL, query);
-    return result;
+  const result = await request(MASTER_URL, query);
+  return result;
 }
 
-const GetBusinessDetail = async(businessSlug) => {
-    const query = gql `
+const GetBusinessDetail = async (businessSlug) => {
+  const query = gql`
     query RestaurantDetail {
   restaurant(where: {slug: "` + businessSlug + `"}) {
     aboutUs
@@ -92,18 +92,18 @@ const GetBusinessDetail = async(businessSlug) => {
   }
 }
 `
-    const result = await request(MASTER_URL, query);
-    return result;
+  const result = await request(MASTER_URL, query);
+  return result;
 
 }
 
-const AddToCart=async(data)=>{
-  const query=gql`
+const AddToCart = async (data) => {
+  const query = gql`
  mutation AddToCart {
   createUserCart(
     data: {email: "`+ data?.email + `", price: "` + data.price + `", productDescription: "` + data.description + `", productImage: "` + data.productImage + `", 
-    productName: "` + data.name +`"
-    restaurant: {connect: {slug: "`+data.restaurantSlug+`"}}}
+    productName: "` + data.name + `"
+    restaurant: {connect: {slug: "`+ data.restaurantSlug + `"}}}
   ) {
     id
   }
@@ -118,10 +118,10 @@ const AddToCart=async(data)=>{
   return result;
 
 }
-const GetUserCart =async(userEmail)=>{
-  const query=gql`
+const GetUserCart = async (userEmail) => {
+  const query = gql`
   query GetUserCart {
-  userCarts(where: {email: "`+userEmail+`"}) {
+  userCarts(where: {email: "`+ userEmail + `"}) {
     id
     price
     productDescription
@@ -142,10 +142,10 @@ const GetUserCart =async(userEmail)=>{
   return result;
 }
 
-const DisconnectRestroFromUserCartItem=async(id)=>{
+const DisconnectRestroFromUserCartItem = async (id) => {
   const query = gql`
 mutation DisconnectRestaurantFromCartItem {
-  updateUserCart(data: {restaurant: {disconnect: true}}, where: {id: "`+id+`"}) {
+  updateUserCart(data: {restaurant: {disconnect: true}}, where: {id: "`+ id + `"}) {
     id
   }
   publishManyUserCarts(to: PUBLISHED) {
@@ -159,7 +159,7 @@ mutation DisconnectRestaurantFromCartItem {
 const DeleteItemFromCart = async (id) => {
   const query = gql`
    mutation DeleteCartItem {
-  deleteUserCart(where: {id: "`+id+`"}) {
+  deleteUserCart(where: {id: "`+ id + `"}) {
     id
   }
 }
@@ -172,12 +172,12 @@ const AddNewReview = async (data) => {
   const query = gql`
    mutation AddNewReview {
   createReview(
-    data: {email: "`+data.email+`",
-      profileImage: "`+ data.profileImage +`", 
-      reviewText: "`+ data.reviewText +`",
-      star: `+ data.star +`,
-      userName: "`+ data.userName +`",
-      restaurant: {connect: {slug: "`+ data.RestroSlug +`"}}}
+    data: {email: "`+ data.email + `",
+      profileImage: "`+ data.profileImage + `", 
+      reviewText: "`+ data.reviewText + `",
+      star: `+ data.star + `,
+      userName: "`+ data.userName + `",
+      restaurant: {connect: {slug: "`+ data.RestroSlug + `"}}}
   ) {
     id
   }
@@ -191,10 +191,10 @@ const AddNewReview = async (data) => {
   return result;
 }
 
-const getRestaurantReviews= async(slug)=>{
-  const query = gql `
+const getRestaurantReviews = async (slug) => {
+  const query = gql`
   query RestaurantReviews {
-    reviews(where: {restaurant: {slug: "`+ slug +`"}},orderBy: publishedAt_DESC) {
+    reviews(where: {restaurant: {slug: "`+ slug + `"}},orderBy: publishedAt_DESC) {
       email
       id
       profileImage
@@ -206,20 +206,20 @@ const getRestaurantReviews= async(slug)=>{
   }
   `
   const result = await request(MASTER_URL, query);
-    return result;
+  return result;
 
 };
-const CreateNewOrder= async(data)=>{
-  const query = gql `
+const CreateNewOrder = async (data) => {
+  const query = gql`
 mutation CreateNewOrder {
   createOrder(
-    data: {email: "`+data.email+`",
-     orderAmount: "`+ data.orderAmount +`", 
-     restaurantName: "`+ data.restaurantName +`",
-      userName: "`+ data.userName +`", 
-      address: "`+ data.address +`",
-       phone: "`+ data.phone +`", 
-       zipCode: "`+ data.zipCode +`"}
+    data: {email: "`+ data.email + `",
+     orderAmount: "`+ data.orderAmount + `", 
+     restaurantName: "`+ data.restaurantName + `",
+      userName: "`+ data.userName + `", 
+      address: "`+ data.address + `",
+       phone: "`+ data.phone + `", 
+       zipCode: "`+ data.zipCode + `"}
   ) {
     id
   }
@@ -233,19 +233,19 @@ mutation CreateNewOrder {
 
 };
 
-const UpdateOrderToAddOrderItems=async(name,price,id,email)=>{
-  const query=gql`
+const UpdateOrderToAddOrderItems = async (name, price, id, email) => {
+  const query = gql`
   mutation UpdateOrderWithDetail {
   updateOrder(
-    data: {orderDetail: {create: {OrderItem: {name: "`+name+`", price: "`+price+`", }}}}
-    where: {id: "`+id+`"}
+    data: {orderDetail: {create: {OrderItem: {name: "`+ name + `", price: "` + price + `", }}}}
+    where: {id: "`+ id + `"}
   ) {
     id
   }
   publishManyOrders(to: PUBLISHED) {
     count
   }
-    deleteManyUserCarts(where: {email: "`+email+`"}) {
+    deleteManyUserCarts(where: {email: "`+ email + `"}) {
       count
     }
 
@@ -259,17 +259,45 @@ const UpdateOrderToAddOrderItems=async(name,price,id,email)=>{
 
 }
 
+const GetUserOrders = async (email) => {
+  const query = gql`
+  query UserOrders {
+    orders(where: {email: "`+email+`"}) {
+      address
+      createdAt
+      email
+      id
+      orderAmount
+      orderDetail {
+        ... on OrderItem {
+          id
+          name
+          price
+        }
+      }
+      phone
+      restaurantName
+      userName
+      zipCode
+    }
+  }
+  `
+  const result = await request(MASTER_URL, query);
+  return result;
+}
+
 
 export default {
-    GetCategory,
-    GetBusiness,
-    GetBusinessDetail,
-    AddToCart,
-    GetUserCart,
-    DisconnectRestroFromUserCartItem,
-    DeleteItemFromCart,
-    AddNewReview,
-    getRestaurantReviews,
-    CreateNewOrder,
-   UpdateOrderToAddOrderItems
+  GetCategory,
+  GetBusiness,
+  GetBusinessDetail,
+  AddToCart,
+  GetUserCart,
+  DisconnectRestroFromUserCartItem,
+  DeleteItemFromCart,
+  AddNewReview,
+  getRestaurantReviews,
+  CreateNewOrder,
+  UpdateOrderToAddOrderItems,
+  GetUserOrders
 }
